2d3d66ceb56dc3cde86138e0274648af686c455c
thesammet <50338990+thesammet@users.noreply.github.com>
2023-06-08 01:52:37 +0300

    addition: redis connect & caching function

--- index.js
+++ index.js
@@ -1,27 +1,67 @@
 const express = require('express');
-
 const app = express();
+const axios = require('axios');
+const redis = require('redis');
 
 const port = 3000;
 
-const axios = require('axios');
+// make a connection to the local instance of Redis
+// with legacy mode and local port 6379
+const client = redis.createClient({
+  legacyMode: true,
+  PORT: 6379
+})
+client.connect().catch(console.error)
 
-app.get('/posts/', async (req, res) => {
+
+// Redis connection event handlers
+client.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+client.on('error', (error) => {
+  console.error('Redis connection error:', error);
+});
+
+app.get('/get-posts/:postitem', async (req, res) => {
+  const postItem = req.params.postitem;
+
   try {
-    const posts = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
-    return res.status(200).send({
-      error: false,
-      data: posts.data
-    });
+    client.get(postItem, async (err, post) => {
+      console.log(post)
 
+      if (post) {
+        res.status(200).send({
+          error: false,
+          message: `POSTS for ${postItem} from the cache`,
+          data: JSON.parse(post)
+        });
+      } else {
+        // If there is no data in cache, send a request to the server.
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postItem}`);
+        const posts = response.data;
+
+        // Save data to cache using setex with postItem id
+        client.setex(postItem, 1440, JSON.stringify(posts));
+
+        res.status(200).send({
+          error: false,
+          message: `Posts for ${postItem} from the server`,
+          data: posts
+        });
+      }
+    });
   } catch (error) {
-    console.log(error)
+    return res.status(500).send({
+      message: error.toString(),
+      error: true,
+      data: null
+    });
   }
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
 
 module.exports = app;
